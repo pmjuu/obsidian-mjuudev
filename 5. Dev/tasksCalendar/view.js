c29c10799ead22f25db10f43aef0c7b5d4fe3dfe
pmjuu <minjupark@Minjuui-MacBookPro.local>
2024-04-22 18:17:03 +0900

    refactor: organize taskTemplate code as html structure

--- 5. Dev/tasksCalendar/view.js
+++ 5. Dev/tasksCalendar/view.js
@@ -50,7 +50,19 @@ var calendarClockIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" heig
 var calendarCheckIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line><path d="m9 16 2 2 4-4"></path></svg>';
 var calendarHeartIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 10V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14c0 1.1.9 2 2 2h7"></path><path d="M16 2v4"></path><path d="M8 2v4"></path><path d="M3 10h18"></path><path d="M21.29 14.7a2.43 2.43 0 0 0-2.65-.52c-.3.12-.57.3-.8.53l-.34.34-.35-.34a2.43 2.43 0 0 0-2.65-.53c-.3.12-.56.3-.79.53-.95.94-1 2.53.2 3.74L17.5 22l3.6-3.55c1.2-1.21 1.14-2.8.19-3.74Z"></path></svg>';
 var cellTemplate = `<div class='cell {{class}}' data-weekday='{{weekday}}'><a class='internal-link cellName' href='${dailyNotePath}/{{dailyNote}}'>{{cellName}}</a><div class='cellContent'>{{cellContent}}</div></div>`;
-var taskTemplate = "<a class='internal-link' href='{{taskPath}}'><div class='task {{class}}' style='{{style}}' title='{{title}}'><div class='inner'><div class='note'>{{note}}</div><div class='icon'>{{icon}}</div><div class='description' data-relative='{{relative}}'>{{taskContent}}</div></div></div></a>";
+var taskTemplate = `
+		<a class='internal-link' href='{{taskPath}}'>
+			<div class='task {{class}}' style='{{style}}' title='{{title}}'>
+				<div class='inner'>
+					<div class='note'>{{note}}</div>
+					<div class='icon'>{{icon}}</div>
+					<div class='description' data-relative='{{relative}}'>
+						{{taskContent}}
+					</div>
+				</div>
+			</div>
+		</a>
+	`;
 const rootNode = dv.el("div", "", {cls: "tasksCalendar "+options, attr: {id: "tasksCalendar"+tid, view: view, style: 'position:relative;-webkit-user-select:none!important'}});
 if (css) { var style = document.createElement("style"); style.innerHTML = css; rootNode.append(style) };
 var taskDoneIcon = "✅";
